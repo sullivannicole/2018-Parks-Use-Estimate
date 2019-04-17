1c11d4d90aacbd0ecbae00d3bedeeae721dc0ab5
Elizabeth Roten <roten.elizabeth@gmail.com>
2019-04-17 11:02:08 -0500

    review with Nicole and make notes

---
+++ PlayFeatures colorValues.R
@@ -0,0 +1,43 @@
+# Color pallette values  
+
+
+## Council Colors
+councilBlue   <- "#0054A4" # Council blue
+cdGreen       <- "#78A22F" # Community Development green
+mtsRed        <- "#EE3124" # MTS red
+esBlue        <- "#009AC7" # Environmental Services blue
+suppYellow    <- "#FFD200"
+suppBlack     <- "#000000"
+suppGray      <- "#666666"
+suppWhite     <- "#FFFFFF"
+
+## Play Features colors
+playGreen    <- "#7BA529"
+playBlue     <- "#005AAD"
+playDaBlue   <- "#00295A"
+playYellow   <- "#C6DE29"
+playLiBlue   <- "#009CCE"
+playSalmon   <- "#E78452"
+
+## Metro Stats colors
+metroBlue    <- "#0875C3"
+metroBrown   <- "#A16C4C"
+metroRed     <- "#A14D5D"
+metroDaPurp  <- "#643967"
+metroMePurp  <- "#AC74A6"
+metroLiPurp  <- "#D8B5D6"
+metroPink    <- "#F6BD9C"
+metroTan     <- "#EAE6C8"
+
+
+## Regional Parks Travel Patterns
+parkGreen    <- "#008537" # the starting color for all parks
+menuBlue     <- "#012b5d" # play features dark blue, background for sidebar menu
+menuHoBlue   <- "#033a7d" # menu item hover color (~2 shades lighter than menuBlue)
+startRed     <- "#f0c0a8" # starting color for TAZ index values
+endRed       <- "#6d3112" # ending color for TAZ index values
+parkYellow   <- "#ffd15d" # yellow for the selected park
+
+
+whiteSmoke   <- "#F5F5F5"
+darkGreen    <- "#547121"
